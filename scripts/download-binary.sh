539ed466defb8f0ba19c9bc7a6731cf7999316fb
Anthony Gitau <gitau.ag@gmail.com>
2025-10-19 13:05:36 +0300

    updated commit

--- scripts/download-binary.sh
+++ scripts/download-binary.sh
@@ -2,7 +2,7 @@
 # Simplified: detect OS/arch, download and extract directly in api-gateway/whatsapp
 set -e
 
-VER="${WHATSAPP_VERSION:-7.5.0}"
+VER="${WHATSAPP_VERSION:-7.7.0}"
 OUT_DIR="dist"
 
 uname_s=$(uname -s)
