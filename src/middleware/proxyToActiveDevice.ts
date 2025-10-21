45c2b6507650b1a651e294327ad24f822187f133
Anthony Gitau <gitau.ag@gmail.com>
2025-10-21 08:20:54 +0300

    nit fix

--- src/middleware/proxyToActiveDevice.ts
+++ src/middleware/proxyToActiveDevice.ts
@@ -105,6 +105,8 @@ const proxyToActiveDevice = asyncHandler(async (req: Request, res: Response, nex
       data: response.data
     };
 
+    console.log("target url", targetUrl)
+
     if (targetUrl != 'http://localhost:8000/app/login') {
         next = undefined; // Prevent calling next middleware except for login route
     }
